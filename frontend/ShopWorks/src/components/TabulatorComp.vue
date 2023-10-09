62397b9b6d226cffbe91d181a1b52ed1613c82fd
OBELISK\Alex <Prothagarus@outlook.com>
2023-10-09 16:30:44 -0400

    Reformat table

--- frontend/ShopWorks/src/components/TabulatorComp.vue
+++ frontend/ShopWorks/src/components/TabulatorComp.vue
@@ -1,47 +1,38 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { TabulatorFull as Tabulator } from 'tabulator-tables'; //import Tabulator library
-import { DateTime } from 'luxon'
-import { type RowComponent, type CellComponent } from 'tabulator-tables'
+import { TabulatorFull as Tabulator } from 'tabulator-tables';
+import { DateTime } from 'luxon';
+import { type RowComponent, type CellComponent } from 'tabulator-tables';
 
 interface ToDoGet {
-    id: string
-    task: string
+    id: string;
+    task: string;
 }
+
 interface Taskdropdown {
-    task: string
+    task: string;
 }
 
 export default defineComponent({
     data() {
         return {
-            tabulator: null, //variable to hold your table
-            tableData: [] as Array<ToDoGet>, //data for table to display
+            tabulator: null,
+            tableData: [] as Array<ToDoGet>,
             taskdropdown: [] as Array<Taskdropdown>,
             savedata: [] as Array<ToDoGet>
-        }
+        };
     },
     methods: {
         gettabledata() {
-            this.tableData = []
-            fetch('http://localhost:8043/ToDoGet').then((d) => d.json().then((d2: Array<ToDoGet>) => { this.tableData = d2 }))
-
+            this.tableData = [];
+            fetch('http://localhost:8043/ToDoGet').then((d) => d.json().then((d2: Array<ToDoGet>) => { this.tableData = d2; }));
         },
         getdropdownlist() {
-            fetch('http://localhost:8043/ToDoGet_DropdownList')
-                .then((response) => response.json())
-                .then((data) => {
-                    this.taskdropdown = data;
-                })
+            this.taskdropdown = [];
+            fetch('http://localhost:8043/ToDoGet_DropdownList').then((d) => d.json().then((d2: Array<Taskdropdown>) => { this.taskdropdown = d2; }));
         },
-        persisttablechangestobuffer() {
-            this.savedata = [] as Array<ToDoGet>
-            let rowdata = new Set<RowComponent>()
-            let buf: Array<CellComponent> = this.tabulator.getEditedCells()
-            buf.forEach((c: CellComponent) => {
-                rowdata.add(c.getRow())
-            })
-            rowdata.forEach((e: RowComponent) => { this.savedata.push(e.getData()) })
+        persisttablechangestobuffer(row: RowComponent) {
+            this.savedata.push(row.getData());
         },
         sendData() {
             fetch('http://localhost:8043/ToDoGet_Save', {
@@ -58,41 +49,39 @@ export default defineComponent({
                 .catch((error) => {
                     console.error('Error:', error);
                 });
-        },
-
-
-
-
-
-
+        }
     },
-
     mounted() {
-        this.taskdropdown = this.getdropdownlist()
-        //instantiate Tabulator when element is mounted
+        this.gettabledata();
+        this.getdropdownlist();
+
         this.tabulator = new Tabulator(this.$refs.table, {
-            data: this.gettabledata(), //link data to table
-            reactiveData: true, //enable data reactivity
-            columns: [{ title: 'id', field: 'id' },
-            {
-                title: 'task',
-                field: 'task',
-                editor: "select",
-                editorParams: () => {
-                    let dropdownOptions = {};
-                    this.taskdropdown.forEach((option) => {
-                        dropdownOptions[option.task] = option.task;
-                    });
-                    return { values: dropdownOptions };
+            data: this.tableData,
+            reactiveData: true,
+            columns: [
+                { title: 'id', field: 'id' },
+                {
+                    title: 'task',
+                    field: 'task',
+                    editor: "select",
+                    editorParams: () => {
+                        let dropdownOptions = {};
+                        this.taskdropdown.forEach((option) => {
+                            dropdownOptions[option.task] = option.task;
+                        });
+                        return { values: dropdownOptions };
+                    }
                 }
-                //define table columns
-            ]
+            ],
+            cellEdited: (cell) => {
+                this.persisttablechangestobuffer(cell.getRow());
+            }
         });
     }
-})
+});
 </script>
 
 <template>
-    <button @click="sendData">Save Data</button>
     <div ref="table"></div>
+    <button @click="sendData">Save Data</button>
 </template>
---
+++ frontend/readme.md
@@ -0,0 +1,3 @@
+
+ChatGPT 4 client conversation on how to finish the tabulator save interface.
+https://www.phind.com/agent?cache=clnjabuih000fl107tcvh7cwe
